76a6d205e1a2e7208cd43511327911e3b6f95a3b
Adela Ahmadi <adela.ahmadi1382@gmail.com>
2026-05-17 11:33:09 +0430

    feat: implement global error handling system

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 const app = express();
 
 app.use(cors());
@@ -14,5 +14,13 @@ app.use(express.json());
 app.get("/", (_req, res) => {
   res.send("CollabFlow API Running...");
 });
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.originalUrl,
+  });
+});
+app.use(globalErrorHandler);
 
 export default app;
---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+
+const globalErrorHandler = (
+  error: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  const statusCode = error.statusCode || 500;
+
+  const message = error.message || "Something went wrong";
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    error,
+  });
+};
+
+export default globalErrorHandler;
