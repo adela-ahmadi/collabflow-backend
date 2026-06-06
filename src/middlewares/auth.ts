497cab1aa8e9a5a6a91611c37fc06d5d8ee886cc
Adela Ahmadi <adela.ahmadi1382@gmail.com>
2026-06-06 14:17:45 +0430

    docs: enable jwt authorization in swagger

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -7,7 +7,7 @@ import config from "../config";
 const auth = (...requiredRoles: string[]) => {
   return async (req: Request, _res: Response, next: NextFunction) => {
     try {
-      const token = req.headers.authorization;
+      const token = req.headers.authorization?.split(" ")[1];
 
       if (!token) {
         throw new AppError(401, "You are not authorized");
